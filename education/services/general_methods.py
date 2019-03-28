817fea6219a0ad9d198e26983f31f99ce5eb8876
Gönül Altun <gonul.altun@komtas.com>
2019-03-28 16:55:42 +0200

    menu

--- education/services/general_methods.py
+++ education/services/general_methods.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import Permission
 
+from education.models import Menu
 
 
-
+def getMenu(request):
+    menus = Menu.objects.all()
+    return {'menus': menus}
 
 
 
--- oxiterp/settings.py
+++ oxiterp/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
+                'education.services.general_methods.getMenu'
 
 
                 #"education.services.general_methods.append_privileges",

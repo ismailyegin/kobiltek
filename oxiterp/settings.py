79c648debfa1f2420bafb9844828737c4f870782
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2019-07-09 14:38:58 +0300

    yoklama

--- oxiterp/settings.py
+++ oxiterp/settings.py
@@ -169,3 +169,13 @@ LOGIN_REDIRECT_URL = '/patient'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+

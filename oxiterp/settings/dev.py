43c737716077e9256f36f60ae7c71497f378cb1b
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2019-07-10 12:53:09 +0300

    Multiple environment

---
+++ oxiterp/settings/dev.py
@@ -0,0 +1,20 @@
+from oxiterp.settings.base import *
+
+# Override base.py settings here
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oxiterp',
+        'USER': 'postgres',
+        'PASSWORD': 'oxit2016',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+try:
+    from oxiterp.settings.local import *
+except :
+    pass
---
+++ oxiterp/settings/prod.py
@@ -0,0 +1,24 @@
+from oxiterp.settings.base import *
+
+# Override base.py settings here
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oxiterp',
+        'USER': 'postgres',
+        'PASSWORD': 'oxit2016',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+try:
+    from oxiterp.settings.local import *
+except :
+    pass

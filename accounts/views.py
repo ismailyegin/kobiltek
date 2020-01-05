ab925c169eb45952d5e9d72de26f9b5cd48fabb2
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2020-01-05 19:51:34 +0300

    Son değişiklikler

--- accounts/views.py
+++ accounts/views.py
@@ -72,11 +72,11 @@ def forgot(request):
             user = obj.save()
             html_content = ''
             subject, from_email, to = 'TWF Bilgi Sistemi Kullanıcı Bilgileri', 'no-reply@twf.gov.tr', obj.email
-            text_content = 'Aşağıda ki bilgileri kullanarak sisteme giriş yapabilirsiniz.'
-            html_content = '<p> <strong>Site adresi:</strong> <a href="http://sbs.twf.gov.tr:81"></a>sbs.twf.gov.tr:81</p>'
+            html_content = '<h2>Aşağıda ki bilgileri kullanarak sisteme giriş yapabilirsiniz.</h2>'
+            html_content = html_content+'<p> <strong>Site adresi:</strong> <a href="http://sbs.twf.gov.tr:81"></a>sbs.twf.gov.tr:81</p>'
             html_content = html_content + '<p><strong>Kullanıcı Adı:</strong>' + obj.username + '</p>'
             html_content = html_content + '<p><strong>Şifre:</strong>' + password + '</p>'
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg = EmailMultiAlternatives(subject, '', from_email, [to])
             msg.attach_alternative(html_content, "text/html")
             msg.send()
 

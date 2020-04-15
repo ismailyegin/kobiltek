ed8a576edbd1468cc95638f733a4442bf95660c8
fatih-berktug <faatihberktug@hotmail.com>
2020-04-15 15:41:21 +0300

    Antrenör son

--- wushu/Forms/GradeForm.py
+++ wushu/Forms/GradeForm.py
@@ -10,7 +10,7 @@ class GradeForm(ModelForm):
     definition = forms.ModelChoiceField(queryset=CategoryItem.objects.filter(forWhichClazz='COACH_GRADE'),
                                         to_field_name='name',
                                         empty_label="Seçiniz",
-                                        label="Tanımı",
+                                        label="Kademe",
                                         widget=forms.Select(
                                             attrs={'class': 'form-control select2 select2-hidden-accessible',
                                                    'style': 'width: 100%; '}))

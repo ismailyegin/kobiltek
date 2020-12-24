cec50d084f0ab0df709cb0093a29c13ee7f2b851
fatih-berktug <faatihberktug@hotmail.com>
2020-12-24 10:10:09 +0300

    Log search

--- wushu/Views/LogViews.py
+++ wushu/Views/LogViews.py
@@ -35,8 +35,15 @@ def return_log(request):
             firstName = user_form.cleaned_data.get('first_name')
             lastName = user_form.cleaned_data.get('last_name')
             email = user_form.cleaned_data.get('email')
+            playDate = request.POST.get('playDate')
+            finishDate = request.POST.get('finishDate')
+            if playDate:
+                playDate = datetime.strptime(playDate, '%d/%m/%Y').date()
 
-            if not (firstName or lastName or email):
+            if finishDate:
+                finishDate = datetime.strptime(finishDate, "%d/%m/%Y").date()
+
+            if not (firstName or lastName or email or playDate or finishDate):
                 logs = Logs.objects.all().order_by('-creationDate')
 
             else:
@@ -47,6 +54,10 @@ def return_log(request):
                     query &= Q(user__first_name__icontains=firstName)
                 if email:
                     query &= Q(user__email__icontains=email)
+                if playDate:
+                    query &= Q(creationDate__gte=playDate)
+                if finishDate:
+                    query &= Q(creationDate__lt=finishDate)
 
                 logs = Logs.objects.filter(query).order_by('-creationDate')
     return render(request, 'Log/Logs.html', {'logs': logs, 'user_form': user_form})
